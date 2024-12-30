e30b4e9544cdbc37df1245172ca4ed1049cbe24d
578munni <zoharakhatun10@gmail.com>
2024-12-30 14:38:51 +0600

    copy

--- dom.js
+++ dom.js
@@ -10,7 +10,28 @@ const generat = () => {
 const update = () => {
   const color = generat();
   const colorBox = document.getElementById('colorBox');
+  const code = document.getElementById('code');
+  
   colorBox.style.backgroundColor = color;
+  code.innerText = color;
+
 }
+
+const copy = () => {
+  const code = document.getElementById('code');
+  const input = document.createElement('input');
+  document.body.appendChild(input);
+  input.value = code.innerText;
+
+  input.select();
+  document.execCommand('copy');
+  document.body.removeChild(input);
+  alert('color copy to clipboard');
+   
+}
+
+document.getElementById('btn').addEventListener('click', ()=> {
+  copy();
+});
 
 update();
